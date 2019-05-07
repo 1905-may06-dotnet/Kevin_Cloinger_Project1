7a0a22cafd390ff408fe11efcf075be6b7ac98bc
kevin cloinger <kevin.cloinger@gmail.com>
2019-05-07 11:41:07 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace cloinger_kevin_stuff
+{
+    class Program
+    {
+        static string name="";
+        static void Main(string[] args)
+        {
+            Console.Write("type something");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello World! "+name);
+        }
+    }
+}
